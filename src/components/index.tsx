7bc717cb1733d6cc8ce37200888b051a83169c53
ElenaRevicheva <elena.revicheva2016@gmail.com>
2025-03-26 15:03:24 -0500

    ✅ Add unified exports for all marketplace components

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,8 +1,9 @@
-export { default as ListingGrid } from "./ListingGrid";
-export { default as DirectListingButton } from "./DirectListingButton";
-export { default as AuctionListingButton } from "./AuctionListingButton";
-export { default as MakeOfferButton } from "./MakeOfferButton";
-export { default as BuyListingButton } from "./BuyListingButton";
 export { default as ApproveButton } from "./ApproveButton";
+export { default as AuctionListingButton } from "./AuctionListingButton";
+export { default as BuyListingButton } from "./BuyListingButton";
+export { default as DirectListingButton } from "./DirectListingButton";
 export { default as Events } from "./Events";
+export { default as ListingGrid } from "./ListingGrid";
+export { default as MakeOfferButton } from "./MakeOfferButton";
 export { default as NFTCard } from "./NFT";
+export { default as PageComponent } from "./page";
